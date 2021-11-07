5a3d06ad5b469f69cb10bb60316c82e8bcc7f7ad
ThaiTan-git <63284265+ThaiTan-git@users.noreply.github.com>
2021-11-07 22:11:46 +0700

    MinorChange

--- trainingcourseapp/TrainingCourseApp/Controllers/TrainerController.cs
+++ trainingcourseapp/TrainingCourseApp/Controllers/TrainerController.cs
@@ -12,75 +12,72 @@ namespace TrainingCourseApp.Controllers
     {
         // GET: Trainer
 
-        public class TrainersController : Controller
+        private ApplicationDbContext _context;
+        public TrainerController()
         {
-            private ApplicationDbContext _context;
-            public TrainersController()
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var userInDb = _context.Trainers
+                .SingleOrDefault(t => t.TrainerId == userId);
+            return View(userInDb);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var trainerInDb = _context.Trainers
+                .SingleOrDefault(t => t.Id == id);
+            if (trainerInDb == null)
             {
-                _context = new ApplicationDbContext();
+                return HttpNotFound();
             }
-            public ActionResult Index()
-            {
-                var userId = User.Identity.GetUserId();
-                var userInDb = _context.Trainers
-                    .SingleOrDefault(t => t.TrainerId == userId);
-                return View(userInDb);
-            }
-            [HttpGet]
-            public ActionResult Edit(int id)
-            {
-                var trainerInDb = _context.Trainers
-                    .SingleOrDefault(t => t.Id == id);
-                if (trainerInDb == null)
-                {
-                    return HttpNotFound();
-                }
-                return View(trainerInDb);
-            }
+            return View(trainerInDb);
+        }
 
-            [HttpPost]
-            public ActionResult Edit(Trainer trainer)
+        [HttpPost]
+        public ActionResult Edit(Trainer trainer)
+        {
+            var trainerInDb = _context.Trainers
+                .SingleOrDefault(t => t.Id == trainer.Id);
+            if (trainerInDb == null)
             {
-                var trainerInDb = _context.Trainers
-                    .SingleOrDefault(t => t.Id == trainer.Id);
-                if (trainerInDb == null)
-                {
-                    return HttpNotFound();
-                }
-                trainerInDb.Name = trainer.Name;
-                trainerInDb.Age = trainer.Age;
-                trainerInDb.Address = trainer.Address;
-                trainerInDb.Specialty = trainer.Specialty;
-
-                _context.SaveChanges();
-                return RedirectToAction("index", "Trainer");
+                return HttpNotFound();
             }
+            trainerInDb.Name = trainer.Name;
+            trainerInDb.Age = trainer.Age;
+            trainerInDb.Address = trainer.Address;
+            trainerInDb.Specialty = trainer.Specialty;
 
-            [HttpGet]
-            public ActionResult GetCourses()
-            {
-                var trainerId = User.Identity.GetUserId();
-                var trainer = _context.Trainers.ToList();
+            _context.SaveChanges();
+            return RedirectToAction("index", "Trainer");
+        }
 
-                var course = _context.Courses
-                    .Include(t => t.Category)
-                    .ToList();
-                var courses = _context.TrainerCourses
-                    .Where(t => t.Trainer.TrainerId == trainerId)
-                    .Select(t => t.Course)
-                    .ToList();
-                return View(courses);
-            }
+        [HttpGet]
+        public ActionResult GetCourses()
+        {
+            var trainerId = User.Identity.GetUserId();
+            var trainer = _context.Trainers.ToList();
 
-            [HttpGet]
-            public ActionResult TraineesInCourse(int id)
-            {
-                var traineeCourse = _context.TraineeCourses
-                    .Where(t => t.CourseId == id)
-                    .Select(t => t.Trainee)
-                    .ToList();
-                return View(traineeCourse);
-            }
+            var course = _context.Courses
+                .Include(t => t.Category)
+                .ToList();
+            var courses = _context.TrainerCourses
+                .Where(t => t.Trainer.TrainerId == trainerId)
+                .Select(t => t.Course)
+                .ToList();
+            return View(courses);
+        }
+
+        [HttpGet]
+        public ActionResult TraineesInCourse(int id)
+        {
+            var traineeCourse = _context.TraineeCourses
+                .Where(t => t.CourseId == id)
+                .Select(t => t.Trainee)
+                .ToList();
+            return View(traineeCourse);
         }
     }
 }
