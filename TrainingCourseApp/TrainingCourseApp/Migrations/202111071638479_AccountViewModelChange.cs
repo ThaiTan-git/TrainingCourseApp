18c3ddeee77d576223f4461e65ea1ae5144064d2
ThaiTan-git <63284265+ThaiTan-git@users.noreply.github.com>
2021-11-08 00:27:25 +0700

    FixBug

---
+++ TrainingCourseApp/TrainingCourseApp/Migrations/202111071638479_AccountViewModelChange.cs
@@ -0,0 +1,16 @@
+namespace TrainingCourseApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AccountViewModelChange : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
